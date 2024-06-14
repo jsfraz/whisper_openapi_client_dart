7f77c5e7cf9b815a2c5ba289e8b00d19aff1c99a
jsfraz <josefraz@seznam.cz>
2024-06-14 17:20:31 +0200

    Updated update.sh

--- update.sh
+++ update.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 VERSION=7.6.0
+OPENAPI_PATH=http://localhost:8080/api/openapi.json
 
 # Downlaod generator
 wget https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/7.6.0/openapi-generator-cli-$VERSION.jar
 # Generate
-# java -jar openapi-generator-cli-$VERSION.jar generate -i ../whisper-server/openapi.json -g dart -c open-generator-config.yaml --enable-post-process-file
-java -jar openapi-generator-cli-$VERSION.jar generate -i http://localhost:8080/api/openapi.json -g dart -c open-generator-config.yaml --enable-post-process-file
+java -jar openapi-generator-cli-$VERSION.jar generate -i $OPENAPI_PATH -g dart -c open-generator-config.yaml --enable-post-process-file
 # Remove generator
 rm openapi-generator-cli-$VERSION.jar
 # Update dependencies
